0411fbac231abb515973ab1223c3dedeb432f234
ushakov <ushakov@mcitys.ru>
2019-10-04 18:11:17 +0300

    Очистка проекта.

--- system/OneScript/bin/1testrunner.bat
+++
@@ -1,2 +0,0 @@
-@oscript.exe "C:\Users\ushakov\Documents\portable\OneScript\lib\1testrunner\main.os" %*
-@exit /b %ERRORLEVEL%
--- system/OneScript/bin/opm.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-setlocal
-
-set lib="%~dp0..\lib"
-set opm=%lib%\opm\src\opm.os
-
-oscript.exe %opm% %*
